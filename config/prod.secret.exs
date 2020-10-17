58f792de352af40a8af62910f29f1738ac41cb44
Chintan Prajapati <chintan9@student.fdu.edu>
2020-10-17 07:31:52 +0000

    Lint fix

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -28,8 +28,8 @@ config :live_view_todo, LiveViewTodoWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
-  check_origin: false,
+  secret_key_base: secret_key_base,
+  check_origin: false
 
 # ## Using releases (Elixir v1.9+)
 #

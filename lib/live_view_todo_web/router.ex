d0af19d5be9f97759d2c0912f81b85e2d31088b5
Chintan Prajapati <chintan9@student.fdu.edu>
2020-10-17 10:46:52 +0000

    test

--- lib/live_view_todo_web/router.ex
+++ lib/live_view_todo_web/router.ex
@@ -32,7 +32,7 @@ defmodule LiveViewTodoWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
+  if Mix.env() do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
